17ad017f4f3242a1a34703ed394af16cec770e18
upron <jani24laaksonen@gmail.com>
2024-12-06 19:14:08 +0200

    Player spawn korjaus

--- 00_Globals/global_level_manager.gd
+++ 00_Globals/global_level_manager.gd
@@ -1,5 +1,6 @@
 extends Node
 
+<<<<<<< Updated upstream
 signal level_load_started
 signal level_loaded
 signal tilemap_bounds_changed( bounds : Array[ Vector2 ] )
@@ -12,11 +13,20 @@ var position_offset : Vector2
 func _ready() -> void:
 	await get_tree().process_frame
 	level_loaded.emit()
+=======
+signal tilemap_bounds_changed( bounds : Array[ Vector2 ] )
+
+var current_tilemap_bounds : Array[ Vector2 ]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+>>>>>>> Stashed changes
 	pass # Replace with function body.
 
 func change_tilemap_bounds( bounds : Array[ Vector2 ] ) -> void:
 	current_tilemap_bounds = bounds
 	tilemap_bounds_changed.emit( bounds )
+<<<<<<< Updated upstream
 	
 func load_new_level(
 		level_path : String,
@@ -47,3 +57,5 @@ func load_new_level(
 	
 	
 	pass
+=======
+>>>>>>> Stashed changes
