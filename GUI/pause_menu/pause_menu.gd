0ff3d623ceeb93c8884c9f555c95fbe393586292
upron <jani24laaksonen@gmail.com>
2024-12-07 21:44:33 +0200

    Inventory save and load
    Osaa seivata inventorin ja loadata sen

--- GUI/pause_menu/pause_menu.gd
+++ GUI/pause_menu/pause_menu.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 signal shown
 signal hidden
 
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 @onready var button_save: Button = $Control/HBoxContainer/Button_save
 @onready var button_load: Button = $Control/HBoxContainer/Button_Load
 @onready var item_description: Label = $Control/ItemDescription
@@ -59,3 +60,7 @@ func _on_load_pressed() -> void:
 
 func update_item_description( new_text : String ) -> void:
 	item_description.text = new_text
+	
+func play_audio( audio : AudioStream ) -> void:
+	audio_stream_player.stream = audio
+	audio_stream_player.play()
