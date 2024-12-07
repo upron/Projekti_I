9ee4d70631b09dfef3e5d8a76b34728f0a2213c3
upron <jani24laaksonen@gmail.com>
2024-12-07 19:38:36 +0200

    Inventorya aloitettu
    UI löytyy mutta sitä pitää hieno säätää

---
+++ GUI/pause_menu/Inventory/Scripts/inventory_ui.gd
@@ -0,0 +1,25 @@
+class_name InventoryUI extends Control
+
+const INVENTORY_SLOT = preload("res://GUI/pause_menu/inventory/inventory_slot.tscn")
+
+@export var data : InventoryData
+
+
+func _ready() -> void:
+	PauseMenu.shown.connect( update_inventory )
+	PauseMenu.hidden.connect( clear_inventory )
+	clear_inventory()	
+	pass
+	
+func clear_inventory() -> void:
+	for c in get_children():
+		c.queue_free()
+		
+func update_inventory( i : int = 0 ) -> void:
+	clear_inventory()
+	for s in data.slots:
+		var new_slot = INVENTORY_SLOT.instantiate()
+		add_child( new_slot )
+		new_slot.slot_data = s
+		
+	get_child( i ).grab_focus()
