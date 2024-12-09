6968242c492c153b3a6c48e0e6c6eb9f43b7a201
AmandaaaaJ <amandajack@hotmail.fi>
2024-12-09 10:44:53 +0200

    boss
    bossin kaikki jutut

---
+++ enemies/Scripts/States/state.gd
@@ -0,0 +1,25 @@
+class_name BossState extends Node
+
+@onready var debug = owner.find_child("debug")
+@onready var player = owner.get_parent().find_child("player")
+@onready var animation_player = owner.find_child("AnimationPlayer")
+
+
+
+func _ready():
+	set_physics_process(false)
+	
+
+
+func enter():
+	set_physics_process(true)
+
+func exit():
+	set_physics_process(false)
+
+func transition():
+	pass
+
+func _physics_process(_delta):
+	transition()
+	debug.text = name
